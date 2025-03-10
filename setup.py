ec9dac9c668e84c93260ff49e8970c41e48bace9
nunupy345345 <airiota@outlook.jp>
2025-03-10 09:53:18 +0900

    [fix][settings]add install requires

--- setup.py
+++ setup.py
@@ -7,5 +7,17 @@ setup(
     author='ota',  # 作者名
     package_dir={"": "src"},  # src以下をpacakgeとして指定
     packages=find_packages(where="src"),  # パッケージの指定
+    install_requires=[  # 必要なパッケージを指定
+        "fastapi",
+        "uvicorn",
+        "websockets",
+        "pypcd",
+        "pytest",
+        "pytest-asyncio",
+        "httpx",
+        "opencv-python",
+        "pypcd4",
+        "bson"
+    ],
     license='MIT'  # ライセンス
 )
